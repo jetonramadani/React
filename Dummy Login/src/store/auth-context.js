7a7b17809660c953bc7d65f464ccc6c3dccc2c4f
Jeton Ramadani <jeton.ramadani@students.finki.ukim.mk>
2023-06-06 14:56:59 +0200

    Dummy Login

---
+++ Dummy Login/src/store/auth-context.js
@@ -0,0 +1,42 @@
+import React, {useState, useEffect} from "react";
+
+const AuthContext = React.createContext({
+  isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
+});
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    const storedLoggedIn = localStorage.getItem("isLogedIn");
+    if (storedLoggedIn === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    setIsLoggedIn(true);
+    localStorage.setItem("isLogedIn", "1");
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLogedIn");
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogin: loginHandler,
+        onLogout: logoutHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
